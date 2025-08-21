0f9774c0618ff5a092702b34b8fee92554a7a37c
umesh <utkrisht@gmail.com>
2025-08-21 13:32:35 +0530

    create category api's and controllers for them

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import AuthRoute from './routes/Auth.route.js';
 import UserRoute from './routes/User.route.js';
+import CategoryRoute from './routes/Category.route.js';
 
 
 dotenv.config();
@@ -29,6 +30,7 @@ app.use(cors({
 // route setup
 app.use('/api/auth' , AuthRoute)
 app.use('/api/user', UserRoute)
+app.use('/api/category', CategoryRoute)
 
 
 
