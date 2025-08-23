e5140b90964961b8b6d3444ba8695eaa302ca63d
umesh <utkrisht@gmail.com>
2025-08-23 06:01:22 +0530

    update the comment controller and make the routes authenticate

--- api/controllers/Comment.controller.js
+++ api/controllers/Comment.controller.js
@@ -20,3 +20,68 @@ export const addcomment = async (req, res, next) => {
         next(handleError(500, error.message))
     }
 }
+
+// this function is to get the comments based on the particular blog id 
+export const getComments = async (req, res, next) => {
+    try {
+        const { blogid } = req.params
+        const comments = await Comment.find({ blogid }).populate('user', 'username avatar').sort({ createdAt: -1 }).lean().exec()
+
+        res.status(200).json({
+            comments
+        })
+    } catch (error) {
+        next(handleError(500, error.message))
+    }
+}
+
+// this function is to show the count of comments in singleBlogDetails page
+export const commentCount = async (req, res, next) => {
+    try {
+        const { blogid } = req.params
+        const commentCount = await Comment.countDocuments({ blogid })
+
+        res.status(200).json({
+            commentCount
+        })
+    } catch (error) {
+        next(handleError(500, error.message))
+    }
+}
+
+// this is for get all the comments that are commented on a user's all blogs and here admin can see all the blogs thorughtout the application
+// but a user with role as user can only see the comments that are maked on the blogs that are created by himseelf only
+export const getAllComments = async (req, res, next) => {
+    try {
+        const user = req.user
+        let comments
+        if (user.role === 'admin') {
+            comments = await Comment.find().populate('blogid', 'title').populate('user', 'username')
+
+        } else {
+
+            comments = await Comment.find({ user: user._id }).populate('blogid', 'title').populate('user', 'username')
+        }
+
+        res.status(200).json({
+            comments
+        })
+    } catch (error) {
+        next(handleError(500, error.message))
+    }
+}
+
+// this is also protected for the user 
+export const deleteComment = async (req, res, next) => {
+    try {
+        const { commentid } = req.params
+        await Comment.findByIdAndDelete(commentid)
+
+        res.status(200).json({
+            success: true,
+            message: 'Data deleted'
+        })
+    } catch (error) {
+        next(handleError(500, error.message))
+    }
+}
--- api/routes/Comment.route.js
+++ api/routes/Comment.route.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { addcomment } from '../controllers/Comment.controller.js';
+import { addcomment, commentCount, deleteComment, getAllComments, getComments } from '../controllers/Comment.controller.js';
+import { authenticate } from '../middleware/authenticate.js'
 
 const CommentRoute = express.Router();
 
-CommentRoute.get('/add', addcomment)
+CommentRoute.get('/add', authenticate,  addcomment)
+CommentRoute.get('/get/:blogid', getComments)
+CommentRoute.get('/get-count/:blogid', commentCount)
+CommentRoute.get('/get-all-comment',authenticate ,  getAllComments)
+CommentRoute.delete('/delete/:commentid', authenticate ,  deleteComment)
+
 
 
 
