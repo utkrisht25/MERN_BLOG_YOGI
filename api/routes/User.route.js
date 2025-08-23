0234d17175e3b600ad0ddc74bbfe9387906505e6
umesh <utkrisht@gmail.com>
2025-08-23 08:29:57 +0530

    created search and all user page UI and merged them with api

--- api/routes/User.route.js
+++ api/routes/User.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { deleteUser, getAllUser, getUser , updateUser } from '../controllers/User.controller.js';
 import upload from '../config/multer.js';
 import { authenticate } from '../middleware/authenticate.js'
-import { onlyadmin } from '../middleware/onlyadmin.js';
+
 
 
 const UserRoute = express.Router()
@@ -10,7 +10,7 @@ const UserRoute = express.Router()
 UserRoute.get('/get-user/:userId',authenticate, getUser);
 UserRoute.put('/update-user/:userId',authenticate, upload.single('file'),updateUser )
 
-UserRoute.get('/get-all-user',onlyadmin, getAllUser)
-UserRoute.delete('/delete/:id',onlyadmin, deleteUser)
+UserRoute.get('/get-all-user',authenticate, getAllUser)
+UserRoute.delete('/delete/:id',authenticate, deleteUser)
 
 export default UserRoute;
